2227be885a818f113d5334f623ccedcc3476c85b
Stjepan Grgic <grgic.stjepan@gmail.com>
2019-07-24 23:19:27 +0200

    Removing styls and cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi </h1>
+    <p>My name is Stjepan Grgic. I can describe myself as a designer because solving problems makes me happy. Above all, I am a curious human being exploring myself and the world through art and travel. This site houses my thoughts and ideas. It also showcases commercial and personal projects I worked on.</p>
+    <Link to="/page-2/">My story</Link>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 

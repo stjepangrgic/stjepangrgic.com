6e9123db3591b22054c962b67c6e6bfc06e8f731
Stjepan Grgic <grgic.stjepan@gmail.com>
2019-07-25 11:14:57 +0200

    text updates, cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
-import Img from "../components/Img"
+import Img from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = (props) =>  {
@@ -14,10 +14,21 @@ const IndexPage = (props) =>  {
       <SEO title="Home" />
       <h1>Hi </h1>
       <p>My name is Stjepan Grgic. I can describe myself as a designer because solving problems makes me happy. Above all, I am a curious human being exploring myself and the world through art and travel. This site houses my thoughts and ideas. It also showcases commercial and personal projects I worked on.</p>
-      <Link to="/page-2/">My story</Link>
+      <Link to="/my-story/">My story</Link>
       <div>
-        <Img fixed={props.data.featuredImage.childImageSharp.fixed} className="featured" />
+        <Img fluid={props.data.featuredImage.childImageSharp.fluid} className="featured" />
       </div>
+
+      <h2>My work</h2>
+      <p>Some of my work. Understanding of color, light, shapes, perspective helped me to go on design road I never have planed understanding of color, light, shapes.</p>
+      <Link to="/work/">View all</Link>
+
+      <h2>Personal projects</h2>
+      <p>Some of my work. Understanding of color, light, shapes, perspective helped me to go on design road I never have planed.</p>
+
+      <h2>Growth</h2>
+      <p>Some of my work. Understanding of color, light, shapes, perspective helped me to go on design road I never have planed.</p>
+      
     </Layout>
   )
 }
@@ -26,8 +37,8 @@ export const query = graphql`
   query {
     featuredImage: file(relativePath: {regex: "/stjepan-grgic-petra-amman-featured/"}) {
       childImageSharp {
-        fixed(width: 1640, quality: 100) {
-          ...GatsbyImageSharpFixed_withWebp
+        fluid(maxWidth: 1640) {
+          ...GatsbyImageSharpFluid_noBase64
           originalName
         }
       }
